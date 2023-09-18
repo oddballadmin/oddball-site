8bb25783e888e1e61bcbaaf75d8a1371e153005b
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2023-09-17 22:50:12 -0400

    commited

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
