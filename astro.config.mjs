cfd6e575df89b3d2ac65a3865adba71f46e3ee24
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2023-09-18 19:41:38 -0400

    fixed an issue in package.json that prevented a sucessful build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  })
+  output: "static",
+  // adapter: node({
+  //   mode: "standalone",
+  // })
 });
