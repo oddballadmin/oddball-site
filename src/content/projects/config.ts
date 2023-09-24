f6d63e4f57fd7055d0371acb63968bfbca9a1d2c
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2023-09-24 17:06:55 -0400

    Added Content pages(.md),overhauled the portfolio page

---
+++ src/content/projects/config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+
+const projectCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    link: z.string().url(),
+    desc: z.string(),
+  })
+})
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'projects': projectCollection,
+  
+};
